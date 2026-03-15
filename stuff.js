7b780836f620a07bb39a206cf28e0b219f4a562e
Manda7848 <manda7848code@gmail.com>
2026-03-15 16:15:44 +0100

    Please Javascript.
    Seeing if I can use commas for the active cell function to make my life easier

--- stuff.js
+++ stuff.js
@@ -3,12 +3,12 @@ window.onload = function(){
     
     
     
-    document.getElementById("wr2").addEventListener("click", Wr2); 
+    document.getElementById("wr2").addEventListener("click", Wr2, Wr1); 
 
 function Wr2() {
     // document.getElementById("a2").style.background-color= #010972;
 
-    window.alert("WR2 Was clicked and I registered it");
+    window.alert("WR2 or WR2 Was clicked and I registered it");
 }
 
 
