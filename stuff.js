9b3a93b6d89a5d0cb85cec2e96218722d4805120
Manda7848 <manda7848code@gmail.com>
2026-03-15 16:12:17 +0100

    Checking if clicks are even being registered

---
+++ stuff.js
@@ -0,0 +1,15 @@
+window.onload = function(){ 
+    
+    
+    
+    
+    document.getElementById("wr2").addEventListener("click", Wr2); 
+
+function Wr2() {
+    // document.getElementById("a2").style.background-color= #010972;
+
+    window.alert("WR2 Was clicked and I registered it");
+}
+
+
+}
