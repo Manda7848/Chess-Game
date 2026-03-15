7aeff072ad67f0d8a9b3375e1134502968691eb4
Manda7848 <manda7848code@gmail.com>
2026-03-15 16:47:53 +0100

    STILL FUMING
    I WAS MISSING QUOTATION MARKS

--- stuff.js
+++ stuff.js
@@ -1,17 +1,23 @@
 window.onload = function(){ 
     
     
-    
-    
-    document.getElementById("wr2").addEventListener("click", Wr2); 
+document.getElementById("wr2").addEventListener("click", Wr2); 
 
 function Wr2() {
     
     // window.alert("A piece Was clicked and I registered it");
     document.getElementById("wr2").style.backgroundColor= "white";
-    console.log("smth was clicked")
+    console.log("White rook 2 was clicked")
 
 }
 
+document.getElementById("wr1").addEventListener("click", Wr1); 
 
+function Wr1() {
+    
+    // window.alert("A piece Was clicked and I registered it");
+    document.getElementById("wr1").style.backgroundColor= "white";
+    console.log("White Rook 1 was clicked")
+
+}
 }
