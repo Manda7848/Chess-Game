dfc318c68050e978514ae90c7f90961754934a00
Manda7848 <manda7848code@gmail.com>
2026-03-20 00:14:09 +0100

    why is the g3 click not working!?

--- stuff.js
+++ stuff.js
@@ -7,8 +7,15 @@ function Wr2() {
     
     // window.alert("A piece Was clicked and I registered it");
     document.getElementById("wr2").style.backgroundColor= "white";
-    console.log("White rook 2 was clicked")
+    console.log("White rook 2 was clicked");
 
+}
+
+document.getElementById("g3").addEventListener("click", ga); 
+
+function ga{
+    document.getElementById("g3").style.backgroundColor= "white";
+    console.log("square 47 was clicked")
 }
 
 document.getElementById("wr1").addEventListener("click", Wr1); 
